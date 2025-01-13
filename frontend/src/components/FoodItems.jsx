5ba022949a1909b11a513e64baf905c082f2f7b2
roopesh-jsp <rupzkumar5@gmail.com>
2025-01-13 21:15:49 +0530

    Untracked all files and applied .gitignore

---
+++ frontend/src/components/FoodItems.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import FoodItem from "./FoodItem";
+import { useCartData } from "../store/CartContext";
+
+export default function FoodItems({ catogorey }) {
+  const data = useCartData();
+
+  return (
+    <div className="foodItems_container">
+      {data.item?.map((item, idx) => {
+        if (catogorey === "all" || catogorey === item.catogery) {
+          return <FoodItem key={idx} data={item} />;
+        }
+      })}
+    </div>
+  );
+}
